10ad4ea7cf42173f8b904e8ea00473a11276de27
Norton Wang <nortantivirus@gmail.com>
2013-12-17 00:56:51 -0500

    add create_post script

--- README.md
+++ README.md
@@ -16,3 +16,11 @@ To use the theme, you simply copy the `theme` folder to your Pelican folder and 
 Alternatively, you can copy the folder to your Pelican directory:
 
     PYTHON_DIR/site-packages/pelican/themes
+
+### Creating a new post
+
+To create a new post about `name`, just do:
+
+    ./create_post name
+    
+This will create the file in the `content` folder with the correct meta tags.
---
+++ create_post.py
@@ -0,0 +1,18 @@
+import sys
+from datetime import datetime
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print 'Usage: ./create_post.py name'
+        sys.exit()
+    else:
+        name = sys.argv[1]
+        now = datetime.now()
+        filename = 'content/{}-{}.md'.format(now.strftime('%Y-%m'), name.lower())
+        with open(filename, 'w') as file:
+            file.write('Title: {}\n'.format(name))
+            file.write('Date: {}\n'.format(now.strftime('%Y-%m-%d %H:%M')))
+            file.write('Tags:\n\n\n')
+
+        print 'Created {}'.format(filename)
+        exit()
